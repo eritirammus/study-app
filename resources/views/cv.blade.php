8d6f9c7ed5032e3e428c62ca612f13791042f9c7
Kevin Zukker <92793367+eritirammus@users.noreply.github.com>
2023-03-15 13:14:04 +0200

    ,,
    ,,

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -3,3 +3,7 @@
         <div class="flex justify-center max-w-7xl mx-auto bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
             <div class="p-5">
                 <iframe src="../../files/CV.pdf" frameborder="0" class="w-96"></iframe>  
+            </div>
+        </div>
+    </div>
+</x-app-layout>
