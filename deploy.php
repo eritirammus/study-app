ea1ab02ad0e8dd9aa2adf0c10135647fb7015d9d
Kevin Zukker <92793367+eritirammus@users.noreply.github.com>
2023-09-13 09:32:14 +0300

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,36 +1,19 @@
 <?php
-
 namespace Deployer;
 
 require 'recipe/laravel.php';
 
-// Project name
-set('application', 'projekti nimi');
-set('remote_user', 'virt109714'); //zone ssh kasutaja nimi
-set('http_user', 'virt109714');  //zone ssh kasutaja nimi
-set('keep_releases', 2);
+// Config
+
+set('repository', 'https://github.com/eritirammus/study-app.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
 
 // Hosts
-host('tak21zukker.itmajakas.ee') // oma zone'i domeen
-    ->setHostname('tak21zukker.itmajakas.ee') // oma zone'i domeen
-    ->set('http_user', 'virt109714') //zone ssh kasutaja nimi
-    ->set('deploy_path', '~/domeenid/www.nimi.itmajakas.ee/portfolio')
-    ->set('branch', 'dev');
 
-// Tasks
-set('repository', 'repo nimi') //vt git clone via ssh;
-//Restart opcache
-task('opcache:clear', function () {
-    run('killall php81-cgi || true');
-})->desc('Clear opcache');
-task('deploy', [
-    'deploy:prepare',
-    'deploy:vendors',
-    'artisan:storage:link',
-    'artisan:view:cache',
-    'artisan:config:cache',
-    'deploy:publish',
-    'opcache:clear',
-    'artisan:cache:clear'
-]);
+
+// Hooks
+
 after('deploy:failed', 'deploy:unlock');
